b192ab4e1b41018675199b9cedd42b9aa98760ec
irisnhu <cuong220851@student.nctu.edu.vn>
2025-10-15 23:37:45 +0700

    fix: sua loi khong lay duoc danh sach nguoi dung

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/userController');
 
 router.get('/users', userController.getUsers);
 router.post('/users', userController.createUser);
+router.put('/users/:id', userController.updateUser);
+router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
